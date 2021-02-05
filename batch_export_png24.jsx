91373fd2401dc1f488785518d9a2aeaad341b64c
Sadi Muhammad Wali <sadiwali@hotmail.com>
2021-02-04 19:56:14 -0500

    first script

---
+++ batch_export_png24.jsx
@@ -0,0 +1,16 @@
+﻿var docs = app.documents;
+var exportOptions = new ExportOptionsPNG24();
+exportOptions.transparency = false;
+exportOptions.artBoardClipping = true;
+exportOptions.verticalScale = 400;
+exportOptions.horizontalScale = 400;
+exportOptions.antiAliasing = true;
+exportOptions.transparency = false;
+
+
+for (var i = 0; i < docs.length; i++) {
+        $.writeln(docs[i]);
+        docs[i].exportFile(new File(docs[i].fullName + "batch"), ExportType.PNG24, exportOptions);
+}
+
+alert("Done.");
