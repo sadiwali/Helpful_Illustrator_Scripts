d57e5bc0a46221f44c82e9359e8fa20a19397fba
Sadi Muhammad Wali <sadiwali@hotmail.com>
2021-02-11 00:01:50 -0500

    fix_layers added

---
+++ Rhino/README.md
@@ -0,0 +1,5 @@
+fix_layers.jsx
+
+---
+
+Just open your exported .ai file in Illustrator first, then run the script.
---
+++ Rhino/fix_layers.jsx
@@ -0,0 +1,40 @@
+﻿var doc = app.activeDocument;
+
+var things = doc.pathItems;
+
+for (var i = 0; i < things.length; i++) {
+    var string_path = things[i].layer.name.split("::");
+    addToLayer(things[i], doc, string_path);
+}
+
+function addToLayer(item, layer_obj, string_path) {
+
+    if (string_path.length == 0) {
+        var old_layer = item.layer.name;
+        item.move(layer_obj, ElementPlacement.PLACEATEND);
+        // remove the empty layer
+        for (var i = 0; i  < doc.layers.length; i++) {
+            var targetLayer = doc.layers[i];
+            if (targetLayer.name ==old_layer) {
+                doc.layers[i].remove();
+            }
+        }
+        return;
+    }
+    var top_layer = string_path[0];
+    for (var i = 0; i < layer_obj.layers.length; i++) {
+        if (layer_obj.layers[i].name == top_layer) {
+            // layer found, recursively add to it
+            string_path.shift();
+            return addToLayer(item, layer_obj.layers[i], string_path);
+        }
+    }
+    
+    // add this layer
+    var new_layer = layer_obj.layers.add();
+    new_layer.name = top_layer;
+    string_path.shift();
+    return addToLayer(item, new_layer, string_path);
+}
+
+alert("Done");
